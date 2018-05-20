38dec1c7fe811a3b6a05975241b13858acf76249
SIKV <skv.sbkr@gmail.com>
2018-05-20 14:20:32 +0300

    Initial commit

---
+++ app/src/main/java/com/github/sikv/photos/view/BaseActivity.kt
@@ -0,0 +1,5 @@
+package com.github.sikv.photos.view
+
+import android.support.v7.app.AppCompatActivity
+
+open class BaseActivity : AppCompatActivity()
---
+++ app/src/main/java/com/github/sikv/photos/viewmodel/BaseViewModel.kt
@@ -0,0 +1,5 @@
+package com.github.sikv.photos.viewmodel
+
+import android.arch.lifecycle.ViewModel
+
+open class BaseViewModel : ViewModel()
