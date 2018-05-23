b54fb176a5abd8554b39d06384b883fa7326784b
SIKV <skv.sbkr@gmail.com>
2018-05-23 20:43:10 +0300

    Open/close search with animation

--- app/src/main/java/com/github/sikv/photos/MainActivity.kt
+++ app/src/main/java/com/github/sikv/photos/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.github.sikv.photos
 
+import android.animation.Animator
 import android.os.Bundle
 import android.support.v4.view.ViewCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.WindowManager
 import com.github.sikv.photos.adapter.PhotoAdapter
+import com.github.sikv.photos.util.AnimUtils
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_main_toolbar.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val ANIMATION_OFFSET = 200
+
+    private var searchVisible = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,5 +39,76 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         })
+
+        init()
+    }
+
+    override fun onBackPressed() {
+        if (searchVisible) {
+            closeSearch()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun init() {
+        mainSearchLayout.y = mainSearchLayout.y - ANIMATION_OFFSET
+
+        mainSearchButton.setOnClickListener {
+           openSearch()
+        }
+
+        mainCloseSearchButton.setOnClickListener {
+            closeSearch()
+        }
+    }
+
+    private fun openSearch() {
+        AnimUtils.animateX(mainFavoritesButton, mainFavoritesButton.x - ANIMATION_OFFSET)
+        AnimUtils.animateX(mainSearchButton, mainSearchButton.x + ANIMATION_OFFSET)
+
+        AnimUtils.animateY(mainSearchLayout, mainSearchLayout.y + ANIMATION_OFFSET, AnimUtils.DURATION_NORMAL,
+                object : Animator.AnimatorListener {
+                    override fun onAnimationRepeat(p0: Animator?) {
+                    }
+
+                    override fun onAnimationEnd(p0: Animator?) {
+//                        if (mainSearchEdit.requestFocus()) {
+//                            window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+//                        }
+                    }
+
+                    override fun onAnimationCancel(p0: Animator?) {
+                    }
+
+                    override fun onAnimationStart(p0: Animator?) {
+                        mainTitleText.visibility = View.INVISIBLE
+                    }
+                })
+
+        searchVisible = true
+    }
+
+    private fun closeSearch() {
+        AnimUtils.animateX(mainFavoritesButton, mainFavoritesButton.x + ANIMATION_OFFSET)
+        AnimUtils.animateX(mainSearchButton, mainSearchButton.x - ANIMATION_OFFSET)
+
+        AnimUtils.animateY(mainSearchLayout, mainSearchLayout.y - ANIMATION_OFFSET, AnimUtils.DURATION_NORMAL,
+                object : Animator.AnimatorListener {
+                    override fun onAnimationRepeat(p0: Animator?) {
+                    }
+
+                    override fun onAnimationEnd(p0: Animator?) {
+                        mainTitleText.visibility = View.VISIBLE
+                    }
+
+                    override fun onAnimationCancel(p0: Animator?) {
+                    }
+
+                    override fun onAnimationStart(p0: Animator?) {
+                    }
+                })
+
+        searchVisible = false
     }
 }
