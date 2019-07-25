03bf9b535f33b8ba464f5e3cc1ea063155c466cd
SIKV <skv.sbkr@gmail.com>
2019-07-25 20:42:02 +0300

    Search shortcut

--- app/src/main/java/com/github/sikv/photos/ui/activity/MainActivity.kt
+++ app/src/main/java/com/github/sikv/photos/ui/activity/MainActivity.kt
@@ -10,6 +10,16 @@ import com.github.sikv.photos.ui.fragment.SearchFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 
+private const val ACTION_SEARCH = "com.github.sikv.photos.action.SEARCH"
+
+private const val PHOTOS_FRAGMENT_INDEX = 0
+private const val SEARCH_FRAGMENT_INDEX = 1
+private const val FAVORITES_FRAGMENT_INDEX = 2
+private const val MORE_FRAGMENT_INDEX = 3
+
+private const val PHOTOS_ITEM_ID = R.id.photos
+private const val SEARCH_ITEM_ID = R.id.search
+
 class MainActivity : BaseActivity() {
 
     private val fragments = listOf(
@@ -19,54 +29,66 @@ class MainActivity : BaseActivity() {
             MoreFragment()
     )
 
-    private var activeFragment = fragments.first()
+    private lateinit var activeFragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
-        setupBottomNavigation()
+        var initialFragmentIndex = PHOTOS_FRAGMENT_INDEX
+        var initialItemId = PHOTOS_ITEM_ID
+
+        if (intent?.action.equals(ACTION_SEARCH)) {
+            initialFragmentIndex = SEARCH_FRAGMENT_INDEX
+            initialItemId = SEARCH_ITEM_ID
+        }
+
+        setupBottomNavigation(initialFragmentIndex, initialItemId)
     }
 
-    private fun setupBottomNavigation() {
+    private fun setupBottomNavigation(initialFragmentIndex: Int, initialItemId: Int) {
+        activeFragment = fragments[initialFragmentIndex]
+
+        mainBottomNavigation.selectedItemId = initialItemId
+
+        fragments.forEachIndexed { index, fragment ->
+            val transaction = supportFragmentManager.beginTransaction()
+                    .add(R.id.mainNavigationContainer, fragment)
+
+            if (index != initialFragmentIndex) {
+                transaction.hide(fragment)
+            }
+
+            transaction.commit()
+        }
+
         mainBottomNavigation.setOnNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.photos -> {
-                    changeFragment(fragments[0])
+                    changeFragment(fragments[PHOTOS_FRAGMENT_INDEX])
                     true
                 }
 
                 R.id.search -> {
-                    changeFragment(fragments[1])
+                    changeFragment(fragments[SEARCH_FRAGMENT_INDEX])
                     true
                 }
 
 
                 R.id.favorites -> {
-                    changeFragment(fragments[2])
+                    changeFragment(fragments[FAVORITES_FRAGMENT_INDEX])
                     true
                 }
 
                 R.id.more -> {
-                    changeFragment(fragments[3])
+                    changeFragment(fragments[MORE_FRAGMENT_INDEX])
                     true
                 }
                 else -> {
                     false
                 }
             }
-        }
-
-        fragments.forEachIndexed { index, fragment ->
-            val transaction = supportFragmentManager.beginTransaction()
-                    .add(R.id.mainNavigationContainer, fragment)
-
-            if (index != 0) {
-                transaction.hide(fragment)
-            }
-
-            transaction.commit()
         }
     }
 
