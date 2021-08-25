52f3fa92d14932954b99ad567c40ec5543b68307
SIKV <skv.sbkr@gmail.com>
2021-08-25 21:36:45 +0300

    Cleanup

--- app/src/main/java/com/github/sikv/photos/config/CacheConfig.kt
+++
@@ -1,5 +0,0 @@
-package com.github.sikv.photos.config
-
-object CacheConfig {
-    const val curatedPhotosCacheTimeoutHours = 12L
-}
--- app/src/main/java/com/github/sikv/photos/config/DbConfig.kt
+++ app/src/main/java/com/github/sikv/photos/config/DbConfig.kt
@@ -1,7 +1,5 @@
 package com.github.sikv.photos.config
 
 object DbConfig {
-    const val curatedPhotosTableName = "CuratedPhotos"
     const val favoritePhotosTableName = "FavoritePhotos"
-    const val remotePagesTableName = "RemotePages"
 }
