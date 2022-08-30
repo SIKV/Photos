103ad84ef3be8fa7b465f864d725c4ef998385b8
SIKV <skv.sbkr@gmail.com>
2022-08-30 22:28:46 +0300

    Update keys.c

--- app/src/main/jni/keys.c
+++ app/src/main/jni/keys.c
@@ -1,16 +1,16 @@
 #include <jni.h>
 
 JNIEXPORT jstring JNICALL
-Java_com_github_sikv_photos_Keys_getPexelsKey(JNIEnv *env, jobject instance) {
+Java_com_github_sikv_photos_Secrets_getPexelsKey(JNIEnv *env, jobject instance) {
     return (*env)-> NewStringUTF(env, "_KEY_");
 }
 
 JNIEXPORT jstring JNICALL
-Java_com_github_sikv_photos_Keys_getUnsplashKey(JNIEnv *env, jobject instance) {
+Java_com_github_sikv_photos_Secrets_getUnsplashKey(JNIEnv *env, jobject instance) {
     return (*env)-> NewStringUTF(env, "_KEY_");
 }
 
 JNIEXPORT jstring JNICALL
-Java_com_github_sikv_photos_Keys_getPixabayKey(JNIEnv *env, jobject instance) {
+Java_com_github_sikv_photos_Secrets_getPixabayKey(JNIEnv *env, jobject instance) {
     return (*env)-> NewStringUTF(env, "_KEY_");
 }
