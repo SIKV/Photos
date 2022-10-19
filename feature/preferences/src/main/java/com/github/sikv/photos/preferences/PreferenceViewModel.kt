a00616168acc780d2d6a8ec56aba0b4dd82f93d2
SIKV <skv.sbkr@gmail.com>
2022-10-19 22:34:01 +0300

    Fix app version is not shown

--- feature/preferences/src/main/java/com/github/sikv/photos/preferences/PreferenceViewModel.kt
+++ feature/preferences/src/main/java/com/github/sikv/photos/preferences/PreferenceViewModel.kt
@@ -1,6 +1,9 @@
 package com.github.sikv.photos.preferences
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.lifecycle.AndroidViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,14 +18,34 @@ sealed interface MoreUiState {
 }
 
 @HiltViewModel
-internal class PreferenceViewModel @Inject constructor() : ViewModel() {
+internal class PreferenceViewModel @Inject constructor(
+    application: Application,
+) : AndroidViewModel(application) {
 
     private val mutableUiState = MutableStateFlow<MoreUiState>(MoreUiState.Empty)
     val uiState: StateFlow<MoreUiState> = mutableUiState
 
     init {
         mutableUiState.value = MoreUiState.Data(
-            appVersion = "" // TODO Fix
+            appVersion = getAppVersion() ?: ""
         )
+    }
+
+    private fun getAppVersion(): String? {
+        return try {
+            val context = getApplication<Application>()
+            val packageInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                context.packageManager.getPackageInfo(
+                    context.packageName,
+                    PackageManager.PackageInfoFlags.of(0)
+                )
+            } else {
+                @Suppress("Deprecation")
+                context.packageManager.getPackageInfo(context.packageName, 0)
+            }
+            packageInfo.versionName
+        } catch (e: Exception) {
+            null
+        }
     }
 }
